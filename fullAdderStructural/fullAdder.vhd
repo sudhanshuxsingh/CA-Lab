1901664c1435d8654ab4093509f90a01c584ce52
Sudhanshu Singh <51945068+s2-sengar@users.noreply.github.com>
2020-05-11 11:50:28 +0530

    Add files via upload

---
+++ fullAdderStructural/fullAdder.vhd
@@ -0,0 +1,84 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:47:05 04/08/2020 
+-- Design Name: 
+-- Module Name:    fullAdder - Structural 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity halfAdder is
+	port(a_h:in STD_LOGIC; 
+		  b_h:in STD_LOGIC;
+		  sum_h:out STD_LOGIC;
+		  carry_h:out STD_LOGIC);
+end halfAdder;
+architecture arch of halfAdder is
+begin
+	sum_h<=a_h xor b_h;
+	carry_h<= a_h and b_h;
+end arch;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity orGate is
+	port(a_o:in STD_LOGIC; 
+		  b_o:in STD_LOGIC;
+		  c_o:out STD_LOGIC);
+end orGate;
+architecture arch of orGate is
+begin
+	c_o<= a_o or b_o;
+end arch;
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity fullAdder is
+    Port ( a : in  STD_LOGIC;
+           b : in  STD_LOGIC;
+           cin : in  STD_LOGIC;
+           s : out  STD_LOGIC;
+           cout : out  STD_LOGIC);
+end fullAdder;
+
+architecture Structural of fullAdder is
+
+component orGate is
+port(a_o:in STD_LOGIC;
+	  b_o:in STD_LOGIC;
+	  c_o:out STD_LOGIC);
+end component;
+
+component halfAdder is
+port(a_h:in STD_LOGIC;
+	  b_h:in STD_LOGIC;
+	  sum_h:out STD_LOGIC;
+	  carry_h:out STD_LOGIC);
+end component;
+
+signal x,y,z:STD_LOGIC;
+
+begin
+
+f0: halfAdder port map(a_h=>a,b_h=>b,sum_h=>x,carry_h=>y);
+f1: halfAdder port map(a_h=>x,b_h=>cin,sum_h=>s,carry_h=>z);
+f2: orGate port map(a_o=>y,b_o=>z,c_o=>cout);
+
+end Structural;
+
---
+++ fullAdderStructural/tb_fullAdder.vhd
@@ -0,0 +1,108 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   14:32:53 04/08/2020
+-- Design Name:   
+-- Module Name:   F:/structural/tb_fullAdder.vhd
+-- Project Name:  structural
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: fullAdder
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_fullAdder IS
+END tb_fullAdder;
+ 
+ARCHITECTURE behavior OF tb_fullAdder IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT fullAdder
+    PORT(
+         a : IN  std_logic;
+         b : IN  std_logic;
+         cin : IN  std_logic;
+         s : OUT  std_logic;
+         cout : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal a : std_logic := '0';
+   signal b : std_logic := '0';
+   signal cin : std_logic := '0';
+
+ 	--Outputs
+   signal s : std_logic;
+   signal cout : std_logic;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+--   constant <clock>_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: fullAdder PORT MAP (
+          a => a,
+          b => b,
+          cin => cin,
+          s => s,
+          cout => cout
+        );
+
+   -- Clock process definitions
+--   <clock>_process :process
+--   begin
+--		<clock> <= '0';
+--		wait for <clock>_period/2;
+--		<clock> <= '1';
+--		wait for <clock>_period/2;
+--   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 10 ns;	
+		a<='0'; b<='0'; cin<='0'; wait for 10 ns;
+     	a<='0'; b<='0'; cin<='1'; wait for 10 ns;
+      	a<='0'; b<='1'; cin<='0'; wait for 10 ns;
+   		a<='0'; b<='1'; cin<='1'; wait for 10 ns;          
+   		a<='1'; b<='0'; cin<='0'; wait for 10 ns;
+   		a<='1'; b<='0'; cin<='1'; wait for 10 ns;
+   		a<='1'; b<='1'; cin<='0'; wait for 10 ns;  
+			a<='1'; b<='1'; cin<='1'; wait for 10 ns;  
+ 
+--      wait for <clock>_period*10;
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
